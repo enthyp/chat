d3372d3c9261fddc565511ec181f9426a0f1e41c
Jakub <kuba.lanecki@gmail.com>
2019-03-23 23:23:44 +0100

    Give credit.

--- app/chat/server.py
+++ app/chat/server.py
@@ -1,3 +1,4 @@
+# practically a copy of https://github.com/wesdoyle/python-twisted-chat-server/blob/master/chatserver.py 
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
